8182b0363657f05e443bfa24d63f0f588ab84c39
Sofia Robb <sofia2341@gmail.com>
2013-05-22 11:55:45 -0700

    run getTranscriptInfo.pl

---
+++ scripts/run_getTranscripts.sh
@@ -0,0 +1,3 @@
+cd ~/src/primerChecker/scripts
+#perl getTranscript.pl /rhome/robb/project/maize_mites/gt150/maize.sqlite
+perl getTranscriptInfo.pl /rhome/robb/project/maize_mites/gt150/maize.sqlite
