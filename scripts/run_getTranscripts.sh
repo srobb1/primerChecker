7a4884e0f24d02a2ce0ebcd9a9976544a6a2a44d
Sofia Robb <sofia2341@gmail.com>
2013-05-24 10:03:19 -0700

    added input check

--- scripts/run_getTranscripts.sh
+++
@@ -1,3 +0,0 @@
-cd ~/src/primerChecker/scripts
-#perl getTranscript.pl /rhome/robb/project/maize_mites/gt150/maize.sqlite
-perl getTranscriptInfo.pl /rhome/robb/project/maize_mites/gt150/maize.sqlite
